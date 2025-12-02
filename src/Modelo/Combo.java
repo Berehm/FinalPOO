d96418688fcb313272fad1b2537ba94689c26987
Berehm <estrellahuertaminaya@gmail.com>
2025-12-01 23:32:45 -0500

    HUERTA MINAYA ESTRELLA BERENICE

--- src/Modelo/Combo.java
+++ src/Modelo/Combo.java
@@ -16,24 +16,13 @@ public class Combo {
         this.id = id;
         this.nombre = nombre;
     }
+
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    
     @Override
-    public String toString(){
-        return this.getNombre();
+    public String toString() {
+        return nombre;
     }
 }
